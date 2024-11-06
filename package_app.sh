dad08ebfd61d3d161dfc16008c5da42f8386240c
Alessandro Poletti <poletti.alessandro0@gmail.com>
2024-11-06 18:16:23 +0100

    Built binaries for x86_64-Windows, arm-MacOS and x86_64-Linux with cross-rs. They are in the './dist' folder

--- package_app.sh
+++ package_app.sh
@@ -1,7 +1,7 @@
 mkdir -p ./dist/RunIGV
 mkdir -p ./dist/RunIGV/Resources
 mkdir -p ./dist/RunIGV/Resources/simple-http-server
-cp    ./target/release/run_igv          ./dist/RunIGV/run_igv
+cp    ./target/release/runIGV           ./dist/RunIGV/runIGV
 cp    ./resources/igvAppIcon.icns       ./dist/RunIGV/Resources/
 cp -r ./resources/igv-webapp            ./dist/RunIGV/Resources/
 cp    ./resources/*simple-http-server*  ./dist/RunIGV/Resources/simple-http-server
